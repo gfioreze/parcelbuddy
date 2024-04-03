e4c4f2a9d7a408b174668811d69569466413a4c9
Gabriel <78756794+gfioreze@users.noreply.github.com>
2024-04-03 18:06:15 +0100

    qr-code_generation

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const qrElements = document.querySelectorAll('.qr-code');
+
+    qrElements.forEach(function(qrElement) {
+
+        const deliveryID = qrElement.id;
+
+        const qrText = "Parcel ID: " + deliveryID;
+        new QRCode(qrElement, {
+            text: qrText,
+            width: 128,
+            height: 128,
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    });
+});
