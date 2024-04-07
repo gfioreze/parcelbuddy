114514c214b4b7a307b32ff49f9836f1e04ab016
Gabriel <78756794+gfioreze@users.noreply.github.com>
2024-04-07 12:24:23 +0100

    some changes

--- get.php
+++ get.php
@@ -1,14 +1,9 @@
 <?php
-
 require_once('Models/DeliveryPointDataSet.php');
 
 header('Content-type: application/json');
-
 $deliveryPointDataSet = new DeliveryPointDataSet();
-
-// Retrieve delivery point information
 $deliveryPoints = $deliveryPointDataSet->getDeliveryPointInfo();
-
 $jsonDeliveryPoints = json_encode($deliveryPoints);
 echo $jsonDeliveryPoints;
 exit();
