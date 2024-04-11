bd0c677bae6a5308083abba249549561488628a2
Gabriel <78756794+gfioreze@users.noreply.github.com>
2024-04-11 21:32:51 +0100

    changes to script/ajax js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         new QRCode(qrElement, {
             text: qrText,
-            width: 128,
-            height: 128,
+            width: 168,
+            height: 168,
             correctLevel: QRCode.CorrectLevel.H
         });
     });
